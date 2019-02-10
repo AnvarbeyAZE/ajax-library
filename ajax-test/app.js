26adf1d79a14758b9ba6eda423e677c4ec300404
Anvarbey Muminov <anvarbey.creative@gmail.com>
2019-02-10 16:57:04 +0500

    test send

---
+++ ajax-test/app.js
@@ -0,0 +1,39 @@
+//Call-backs  test  !! for creating custom-oriented ajax library!!
+
+const posts = [
+  {
+    name : "Anvarbey Muminov",
+  },
+  {
+    name : "NiHao",
+  },
+  {
+    name : "Huy Plin",
+  },
+  {
+    name : "Xuyao Date",
+  },
+  {
+    name : "Xuy Sin lin",
+  }
+];
+function createPost(post,callback){
+  setTimeout(function(){
+   // console.log(post);
+   posts.push(post);
+   callback();
+  },2000);
+
+}
+function getPost(){
+  setTimeout(function(){
+    let output = '';
+    posts.forEach(function(obj){
+        output += `<li>${obj.name}</li>`;
+    });
+    document.body.innerHTML = output;
+  },1000)
+}
+
+
+createPost({name:'Bey'},getPost);
